e6b4626556f6f7b3e444d2e51c5badbae073620a
fishyu <fishyuemail@163.com>
2020-05-24 11:16:14 +0800

    修补提交

---
+++ helloword.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+int main(void)
+{
+   string str="afafafa";
+   cout << str << endl;
+
+  
+   int pause; cin >> pause;
+   return 0;
+}
